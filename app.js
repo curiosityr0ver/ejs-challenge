f116732e80848d5ecba1e41d17686a569dda77a8
Ishu Mehta <>
2021-12-19 20:59:24 +0530

    removed dashes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -25,25 +25,9 @@ app.get("/", function (req, res) {
 })
 
 app.get("/blogs/:params", function (req, res) {
-  var found = -1;
-  for (let i = 0; i < postlist.length; i++) {
-    if (postlist[i].head == req.params.params) {
-      found = i;
-      break;
-    }  
-  }
-  if (found >= 0) {
-    res.render("blog", { postlist: postlist, index: found + 1 }
-    )
-  } else {
-    res.send("oops !");
-  }
-  
-  res.render("blog", {
-    postlist: postlist,
-    index: req.params.params
-
-  })
+  var str = req.params.params;
+  str = str.replace(/-/g, '');
+  console.log(str);
 })
 
 app.get("/contact", function (req, res) {
