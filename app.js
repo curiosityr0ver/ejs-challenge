9a7a810f89a6266e1e29c6656776f11a3e7518e5
Ishu Mehta <>
2022-01-02 13:03:04 +0530

    added dynamic port

--- app.js
+++ app.js
@@ -122,11 +122,15 @@ app.get("/compose", function (req, res) {
 })
 
 app.post("/compose", function (req, res) {
-  Para.create({ head: req.body.composeTitle, body: req.body.composePost }, (err, doc) => {})
+  Para.create({ head: req.body.composeTitle, body: req.body.composePost }, (err, doc) => { })
   res.redirect("/");
 })
 
-app.listen(3000, function () {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port == 3000;
+}
+app.listen(port, function () {
   console.log("Server started on port 3000");
 });
 
